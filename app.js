08fdf7f99d95b793959d6696f90d71c5ac8eddcf
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-09-04 21:10:28 +0530

    Linked stylesheets and express setup is made

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
+// const moment = require('moment')
+// const fileType = require('file-type')
+var express = require('express')
+var app = express()
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'));
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.render('form')
+})
+app.listen(3000,(err,next) => {
+    console.log("Listening to the port 3000")
+    if (err) {
+        console.log(err)
+        next()
+    }
+})
