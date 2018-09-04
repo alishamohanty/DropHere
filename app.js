f7163c7fab1a9258299fd464fa6ae3ccf681e76f
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-09-04 22:07:43 +0530

    Frontend design

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 'use strict'
 const AWS = require('aws-sdk')
-const s3 = new AWS.S3()
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  });
 // const moment = require('moment')
 // const fileType = require('file-type')
 var express = require('express')
